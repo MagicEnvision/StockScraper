3eac3b0158069e6d5156d37ec5fb84a5c160ca84
magicenvision <rjrose2003@gmail.com>
2026-04-02 22:21:41 -0400

    working

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8081;
+
+const TSX_URL = 'https://app-money.tmx.com/graphql';
+const GET_NEWS_AND_EVENTS_QUERY = `query getNewsAndEvents($symbol: String!, $page: Int!, $limit: Int!, $locale: String!, $companyInNews: Boolean) {  news: getNewsForSymbol(    symbol: $symbol    page: $page    limit: $limit    locale: $locale    companyInNews: $companyInNews  ) {    headline    datetime    source    newsid    summary    __typename  }  events: getUpComingEventsForSymbol(symbol: $symbol, locale: $locale) {    title    date    status    type    __typename  }}`;
 
 
 
@@ -11,15 +14,16 @@ const port = 8080;
 // vars = {companyInNews: false, limit: 10, locale: "en", page: 1, symbol: "QIMC"}
 
 app.post('/', (req, res) => {
+
   axios({
     method: 'post',
     allowAbsoluteUrls: false,
-    url: 'https://app-money.tmx.com/graphql',
+    url: TSX_URL,
     headers: {
                 'Content-Type': 'application/json'
     },
     data: {
-        query: `query getNewsAndEvents($symbol: String!, $page: Int!, $limit: Int!, $locale: String!, $companyInNews: Boolean) {  news: getNewsForSymbol(    symbol: $symbol    page: $page    limit: $limit    locale: $locale    companyInNews: $companyInNews  ) {    headline    datetime    source    newsid    summary    __typename  }  events: getUpComingEventsForSymbol(symbol: $symbol, locale: $locale) {    title    date    status    type    __typename  }}`,
+        query: GET_NEWS_AND_EVENTS_QUERY,
         variables: {
             companyInNews: false,
             limit: 10,
@@ -28,7 +32,6 @@ app.post('/', (req, res) => {
             symbol: "QIMC:CNX"
         }
     }  
-    
 })
 .then((response) => {
     console.log(`${axios.defaults.baseURL}`); // Logs the full URL of the request
